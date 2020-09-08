854ae2888653c2dad00f45d2a71df73817315f3f
Ramon Cabral <rrcm16@gmail.com>
2020-09-08 08:36:07 -0400

    refactored todo item table check

--- lists/tests.py
+++ lists/tests.py
@@ -6,3 +6,9 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        item_text = 'A new list item'
+        response = self.client.post('/', data={'item_text': item_text})
+        self.assertIn(item_text, response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
--- lists/views.py
+++ lists/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'home.html')
+    new_item_text = request.POST.get('item_text', '')
+    return render(request, 'home.html', {'new_item_text': new_item_text})
